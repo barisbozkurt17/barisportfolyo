d6be344b4373f9ef626c4cae4429727f46c22c5a
BARIŞ BOZKURT <53062268+barisbozkurt17@users.noreply.github.com>
2025-05-17 00:58:04 +0300

    Delete rc directory

--- rc/App.js
+++
@@ -1,7 +0,0 @@
-            <div className="md:w-1/2 flex justify-center">
-              <img
-                src="/src/images/ceng.svg"
-                alt="Developer"
-                className="w-full max-w-lg animate-float"
-              />
-            </div> 
